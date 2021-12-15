17c65fa8bb33f7fe995f4d6744b7c16701d65f19
Tejaswani Agrawal <83462185+tejaswani23@users.noreply.github.com>
2021-12-15 15:34:25 +0530

    Add files via upload

---
+++ min sum of subarray in i to n range.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int kadane(int a[],int k,int n)
+{
+	int m=a[k],curr=a[k];
+	for(int i=k+1;i<n;i++)
+	{
+		curr=max(a[i],curr+a[i]);
+		m=max(m,curr);
+	}
+	return m;
+}
+int main()
+{
+	int n;
+	cin>>n;
+	int a[n];
+	for(int i=0;i<n;i++)
+	cin>>a[i];
+	for(int i=0;i<n;i++)
+	a[i]=-a[i];
+	for(int i=0;i<n;i++)
+	{
+		int r=kadane(a,i,n);
+		cout<<-r<<" ";
+	}
+}
